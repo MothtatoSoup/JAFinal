3276fba91b4759f0e31288b2f5bac6967a5c1668
MothtatoSoup <maddy.puryear@outlook.com>
2025-04-30 13:47:08 -0400

    did a bunch of stuff, can't do mre stuff

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-// Stuff to add:
-
-// HTML
-// Prority drop down
-
 // Java Script
+// Done button functionality
 // Delete button to delete from list - Add Functionality
-// Done checkmark box - Add Functionality
 // Figure out how to format task Manage (Spacing bwten variables)
 
 // CSS
@@ -15,39 +10,64 @@
 
 // Bugs
 // HTML not on one line
-// The task list updates all tasks values to the newest task
+// The task list overwrites all tasks values to the newest task
 // CSS isn't CSSing
 
 
+// I can't get the individual tasks because the list keeps overwriting (No done, delte or specific styling)
+// I can't stop it from overwrting because it doesn't work then
+// I can't even do css because my browser doesn't recognize it as css
 
 
+try{
 // Gets all the stuff
 let taskManage = document.getElementById("taskmanager"); // The Div
 let task = document.getElementById("task"); // The text input
-let importYN = document.getElementById("checkBox"); // The check box
+let importYN = document.getElementById("checkbox"); // The check box
 let button = document.getElementById("subButt"); // thesubmit button
-let priority = document.getElementById("priorities")
-x = 0;
-
-button.addEventListener("click", addTask(task.value));
+let priority = document.getElementById("priorities") // the priorities things
+let isImport = 0
+let x = 0;
 
 // The tasks
-let taskList = [
-    
-];
+let taskList = [0];
+let newTask = {}
 
-function addTask(thing){
-    thing = {}
+button.addEventListener("click", addTask);
+
+function addTask(){
+    newTask.value = {}
+
+    if(importYN.checked){ // checks if important
+        isImport = "Y"
+    }else{
+        isImport = "N"
+    }
 
     // Defines the task
-    thing.id = x += 1;
-    thing.name = task.value;
-    thing.priority = priority.value;
-    thing.important = 
-    thing.complete = "N";
-    thing.date = "test";
-
-    taskList.push(thing); // can not access task list b4 intit?????
-    taskManage.innerHTML += `<input type="checkbox" class="doneTask">${thing.id} ${thing.name}  Priority:${thing.priority} Important:${thing.important} ${thing.date} <button type="button" class="delete">X</button></br>`; // Adds the task to the div
+    newTask.id = x += 1;
+    newTask.name = task.value;
+    newTask.priority = priority.value;
+    newTask.important = isImport
+    newTask.complete = "N";
+    newTask.date = "test";
+    
+    taskList.push(newTask) // adds task to back of list
+    // Appends the task and stuff to the dive
+    taskManage.innerHTML += `<input type="checkbox" id="doneTask${newTask.id}">${newTask.id} ${newTask.name}  Priority:${newTask.priority} Important:${newTask.important} ${newTask.date} <button type="button" class="delete">X</button></br>`; // Adds the task to the div
     console.log(JSON.stringify(taskList)); // Logs a string of the whole list of tasks
 };
+
+
+
+// let done = document.getElementByClassName(`doneTask${newTask.id}`) 
+// done.addEventListener("click", function(){ // Not working
+//     if(done.checked){ 
+//         console.log("checked")
+//     }
+// })
+
+
+}catch(error){
+    console.log(error)
+}
